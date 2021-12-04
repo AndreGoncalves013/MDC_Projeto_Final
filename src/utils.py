0f0b8f3d7f0d5d036bebac32b1dfbef3ea97f962
André Gonçalves <andredesouzag@gmail.com>
2021-12-04 10:57:47 -0300

    Correcao do utils.py e separando em treino, validacao e teste para imagens

--- src/utils.py
+++ src/utils.py
@@ -1,9 +1,13 @@
 import numpy as np
 import pandas as pd
 
-def get_features_target_data(df, indexes_path):
+def get_features_target_data(df_original, indexes_path):
 
+    df = df_original.copy()
     indexes = pd.read_csv(indexes_path)
+
+    df['filename'] = df.filename.apply(lambda x: x.replace('.', '')[:-3])
+    indexes['filename'] = indexes.filename.apply(lambda x: x.replace('.', '')[:-3])
 
     filtered_df = df.set_index('filename').join(indexes[['filename']].set_index('filename'), how = 'inner').reset_index()
 
@@ -12,10 +16,20 @@ def get_features_target_data(df, indexes_path):
 
     return X, y
 
-def get_train_val_set_data(df):
+def print_shape(X, y, label):
+    print(f'-- {label} --')
+    print('X (shape): ', X.shape)
+    print('y: ', len(y))
 
-    X_train, y_train = get_features_target_data(df, '../dataset/Data/train_indexes.csv')
-    X_val, y_val = get_features_target_data(df, '../dataset/Data/val_indexes.csv')
-    X_test, y_test = get_features_target_data(df, '../dataset/Data/test_indexes.csv')
+def get_train_val_set_data(df_original):
+
+    X_train, y_train = get_features_target_data(df_original, '../dataset/Data/train_indexes.csv')
+    print_shape(X_train, y_train, 'Treino')
+
+    X_val, y_val = get_features_target_data(df_original, '../dataset/Data/val_indexes.csv')
+    print_shape(X_val, y_val, 'Validação')
+    
+    X_test, y_test = get_features_target_data(df_original, '../dataset/Data/test_indexes.csv')
+    print_shape(X_test, y_test, 'Teste')
 
     return X_train, y_train, X_val, y_val, X_test, y_test
